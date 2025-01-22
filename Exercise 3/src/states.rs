5433c4f0745510fff0d4a77d1f84567e21c46774
0 <0>
2025-01-22 15:17:01 +0100

    ex 3

--- Exercise 3/src/states.rs
+++ Exercise 3/src/states.rs
@@ -1,23 +1,23 @@
 const NUMBER_OF_FLOORS: usize = 4;
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum States {
     Idle, Moving, DoorOpen, OutOfOrder
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub enum Direction {
     Up, Down, Stopped,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct Order {
     pub outside_call_up: bool,
     pub outside_call_down: bool,
     pub inside_call: bool,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub struct Elevator{
     pub state: States,
     pub direction: Direction,
@@ -34,17 +34,21 @@ impl Elevator {
             orders: [Order { outside_call_down: false, outside_call_up: false, inside_call: false }; NUMBER_OF_FLOORS],
         }
     }
-    pub fn orders_below(self) -> bool {
+    pub fn orders_below(&self) -> bool {
         self.orders[..self.floor as usize]
             .iter()
             .any(|order| order.inside_call || order.outside_call_down || order.outside_call_up)
     }
-    pub fn orders_above(self) -> bool {
+    pub fn orders_above(&self) -> bool {
         self.orders[self.floor as usize + 1..]
             .iter()
             .any(|order| order.inside_call || order.outside_call_down || order.outside_call_up)
     }
-    pub fn should_stop(self) -> bool {
+    pub fn orders_here(&self) -> bool {
+        let order = self.orders[self.floor as usize];
+        order.inside_call || order.outside_call_down || order.outside_call_up
+    }
+    pub fn should_stop(&self) -> bool {
         match self.direction {
             Direction::Down => return 
                 self.orders[self.floor as usize].outside_call_down ||
@@ -56,5 +60,10 @@ impl Elevator {
                 !self.orders_above(),
             _ => return true,
         }
+    }
+    pub fn clear_order_here(&mut self) {
+        self.orders[self.floor as usize].inside_call = false;
+        self.orders[self.floor as usize].outside_call_down = false;
+        self.orders[self.floor as usize].outside_call_up = false;
     }
 }

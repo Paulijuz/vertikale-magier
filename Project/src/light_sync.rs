0d462752e619ca4dad480ba8217072b06e265d88
Paulius Juzenas <pauliusj@stud.ntnu.no>
2025-02-25 14:49:31 +0100

    refactor: use constatns in sync lights

--- Project/src/light_sync.rs
+++ Project/src/light_sync.rs
@@ -1,12 +1,12 @@
 use crate::elevator_controller::ElevatorRequests;
-use driver_rust::elevio::elev::Elevator;
+use driver_rust::elevio::elev::{Elevator, CAB, HALL_DOWN, HALL_UP};
 
 pub fn sync_call_lights(elevator: &Elevator, requests: &ElevatorRequests) {
     for (floor, request) in requests.iter().enumerate() {
         let floor = floor as u8;
 
-        elevator.call_button_light(floor, 0, request.hall_up);
-        elevator.call_button_light(floor, 1, request.hall_down);
-        elevator.call_button_light(floor, 2, request.cab);
+        elevator.call_button_light(floor, HALL_UP, request.hall_up);
+        elevator.call_button_light(floor, HALL_DOWN, request.hall_down);
+        elevator.call_button_light(floor, CAB, request.cab);
     }
 }

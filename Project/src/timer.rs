6916de7326e450bd7c57a8c8bf7057a2ff893bf3
Paulius Juzenas <pauliusj@stud.ntnu.no>
2025-03-27 16:15:20 +0100

    fix: timer timing all the time

--- Project/src/timer.rs
+++ Project/src/timer.rs
@@ -1,6 +1,9 @@
 use crossbeam_channel as cbc;
 use log::debug;
-use std::{thread::{spawn, JoinHandle}, time::{Duration, Instant}};
+use std::{
+    thread::{spawn, JoinHandle},
+    time::{Duration, Instant},
+};
 
 const DEADLINE_POLL_INTERVAL: Duration = Duration::from_millis(10);
 
@@ -23,11 +26,8 @@ impl Timer {
         let (timeout_channel_tx, timeout_channel_rx) = cbc::unbounded::<()>();
         let (timer_command_channel_tx, timer_command_channel_rx) = cbc::unbounded::<TimerCommand>();
 
-        let thread_handle = spawn(move || timer_loop(
-            duration,
-            timer_command_channel_rx,
-            timeout_channel_tx,
-        ));
+        let thread_handle =
+            spawn(move || timer_loop(duration, timer_command_channel_rx, timeout_channel_tx));
 
         Timer {
             timeout_channel_rx,
@@ -40,12 +40,16 @@ impl Timer {
     ///
     /// **Note:** Calling start on an already started timer will have no effect.
     pub fn start(&mut self) {
-        self.timer_command_channel_tx.send(TimerCommand::Start).unwrap();
+        self.timer_command_channel_tx
+            .send(TimerCommand::Start)
+            .unwrap();
     }
 
     /// Stops the timer if it is running. Has no effect otherwise.
     pub fn stop(&mut self) {
-        self.timer_command_channel_tx.send(TimerCommand::Stop).unwrap();
+        self.timer_command_channel_tx
+            .send(TimerCommand::Stop)
+            .unwrap();
     }
 
     /// Forces the timer to begin counting down from the beginning.
@@ -62,14 +66,18 @@ impl Timer {
 
 impl Drop for Timer {
     fn drop(&mut self) {
-        debug!("Shutting dow timer...");
+        debug!("Shutting down timer...");
+
+        self.timer_command_channel_tx
+            .send(TimerCommand::Exit)
+            .expect("Command channel end should not be dropped yet.");
 
         self.thread_handle
             .take()
             .expect("Timer thread handle should be available when dropping.")
             .join()
             .expect("Timer thread should join without errors.");
-    
+
         debug!("Timer shut down.");
     }
 }
@@ -78,14 +86,14 @@ fn timer_loop(
     duration: Duration,
     timer_command_channel_rx: cbc::Receiver<TimerCommand>,
     timeout_channel_tx: cbc::Sender<()>,
-) { 
+) {
     let ticker = cbc::tick(DEADLINE_POLL_INTERVAL);
     let mut deadline: Option<Instant> = None;
 
     loop {
         cbc::select! {
             recv(timer_command_channel_rx) -> command => {
-                let command = command.unwrap();
+                let command = command.expect("Command channel should exist as long as this thread runs.");
 
                 match command {
                     TimerCommand::Start if deadline.is_none() => {
@@ -98,9 +106,13 @@ fn timer_loop(
                 };
             },
             recv(ticker) -> _ => {
-                if Some(Instant::now()) > deadline {
+                let Some(deadline_instant) = deadline else {
+                    continue;
+                };
+
+                if Instant::now() > deadline_instant {
                     deadline = None;
-                    timeout_channel_tx.send(()).unwrap();
+                    timeout_channel_tx.send(()).expect("Timeout channel receiver should exist as long as this thread runs.");
                 }
             }
         }
